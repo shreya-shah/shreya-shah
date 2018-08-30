b0310dcafc4001fc611655e9901f5ada17d436e6
shreya-shah <shreyashah1920@gmail.com>
2018-08-30 11:55:46 +0530

    changes to javascript

--- WritersBlock/js/index.js
+++ WritersBlock/js/index.js
@@ -4,21 +4,36 @@ var menu_icon,nav,nav_items;
 function initAll(){
     getAllElements();
     assignFunctions();
+    setInterval(showNavBar,500);
 }
 
 function getAllElements(){
     menu_icon = document.getElementById('menu-icon');
     nav = document.getElementById('nav');
+    menu_icon.toggle = 'off';
     nav_items = document.getElementsByClassName('nav-item');
 }
 
 function assignFunctions(){
     menu_icon.onclick = function(){
-        if(nav.style.display === 'none'){
+        if(menu_icon.toggle === 'off'){
             nav.style.display = 'block';
+            menu_icon.toggle = 'on';
         }else{
             nav.style.display = 'none';
+            menu_icon.toggle = 'off';
         }
-    };
+    }; 
+}
 
+function showNavBar(){
+    // alert(window.innerWidth);
+    if(window.innerWidth>800){
+        nav.style.display = 'inline-block';
+        nav.style.position = 'fixed';
+        nav.style.marginTop = '0.4em';
+        nav.style.right = '20em';
+    }else if(menu_icon.toggle === 'off'){
+        nav.style.display = 'none';
+    }
 }
